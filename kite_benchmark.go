c9e27620276fea36b858603f33fad311f043db81
blackbeans <blackbeans.zc@gmail.com>
2015-01-21 18:08:24 +0800

    增加benchmark程序 	new file:   kite_benchmark.go

---
+++ kite_benchmark.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"go-kite/client"
+	"go-kite/protocol"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+func buildStringMessage() *protocol.StringMessage {
+	//创建消息
+	entity := &protocol.StringMessage{}
+	entity.Header = &protocol.Header{
+		MessageId:   proto.String("1234567"),
+		Topic:       proto.String("trade"),
+		MessageType: proto.String("pay-succ"),
+		ExpiredTime: proto.Int64(13700000000),
+		GroupId:     proto.String("go-kite-test"),
+		Commited:    proto.Bool(true)}
+	entity.Body = proto.String("hello go-kite")
+
+	return entity
+}
+
+func main() {
+	//开始向服务端发送数据
+	kclient := client.NewKitClient("localhost:13800", "/user-service", "123456")
+	c := *flag.Int("c", 10, "concurrent")
+	flag.Parse()
+	wg := &sync.WaitGroup{}
+	count := int32(0)
+	lc := int32(0)
+
+	fc := int32(0)
+	flc := int32(0)
+
+	go func() {
+		for {
+
+			tmp := count
+			ftmp := fc
+
+			time.Sleep(1 * time.Second)
+			fmt.Printf("tps:%d/%d", (tmp - lc), (ftmp - flc))
+			lc = tmp
+			flc = ftmp
+		}
+	}()
+
+	wg.Add(c)
+	for i := 0; i < c; i++ {
+		go func() {
+			for {
+				err := kclient.SendMessage(buildStringMessage())
+				if nil != err {
+					fmt.Printf("SEND MESSAGE |FAIL|%s\n", err)
+					atomic.AddInt32(&fc, 1)
+				} else {
+					atomic.AddInt32(&count, 1)
+				}
+			}
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+}
