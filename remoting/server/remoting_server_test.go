508d78783fa67efbef448400e0a7cac874cecced
孟君 <mengjun1990@gmail.com>
2015-02-02 22:27:28 +0800

    merge

--- remoting/server/remoting_server_test.go
+++ remoting/server/remoting_server_test.go
@@ -19,7 +19,7 @@ type defaultStore struct {
 }
 
 func (self *defaultStore) Query(messageId string) *store.MessageEntity {
-	entity := store.NewStringMessageEntity(buildStringMessage())
+	entity := store.NewStringMessageEntity(buildStringMessage(messageId))
 	return entity
 
 }
@@ -36,14 +36,14 @@ func (self *defaultStore) UpdateEntity(entity *store.MessageEntity) bool {
 	return true
 }
 
-func buildStringMessage() *protocol.StringMessage {
+func buildStringMessage(id string) *protocol.StringMessage {
 	//创建消息
 	entity := &protocol.StringMessage{}
 	entity.Header = &protocol.Header{
-		MessageId:   proto.String("1234567"),
+		MessageId:   proto.String("1234567_" + id),
 		Topic:       proto.String("trade"),
 		MessageType: proto.String("pay-succ"),
-		ExpiredTime: proto.Int64(13700000000),
+		ExpiredTime: proto.Int64(time.Now().Unix()),
 		GroupId:     proto.String("go-kite-test"),
 		Commited:    proto.Bool(true)}
 	entity.Body = proto.String("hello go-kite")
@@ -52,16 +52,20 @@ func buildStringMessage() *protocol.StringMessage {
 }
 
 //初始化存储
-// var kitestore = store.NewKiteMysql("root:root@tcp(localhost:8889)/kite")
 var kitestore = &defaultStore{}
 var ch = make(chan bool, 1)
-
-// var kclient *core.KiteClient
 var manager *client.KiteClientManager
 var remotingServer *RemotingServer
 
 func init() {
 
+	initKiteServer()
+	log.Println("KiteServer|START|....")
+	initKiteClient()
+	log.Println("KiteClient|START|....")
+}
+
+func initKiteServer() {
 	//初始化pipeline
 	pipeline := pipe.NewDefaultPipeline()
 	clientManager := rclient.NewClientManager()
@@ -75,7 +79,7 @@ func init() {
 	pipeline.RegisteHandler("deliver", handler.NewDeliverHandler("deliver", kitestore))
 	pipeline.RegisteHandler("remoting", handler.NewRemotingHandler("remoting", clientManager))
 
-	remotingServer = NewRemotionServer("localhost:13801", 3*time.Second,
+	remotingServer = NewRemotionServer("localhost:13800", 3*time.Second,
 		func(remoteClient *rclient.RemotingClient, packet []byte) {
 			event := pipe.NewPacketEvent(remoteClient, packet)
 			err := pipeline.FireWork(event)
@@ -95,12 +99,14 @@ func init() {
 		}
 	}()
 
-	exchanger.PushQServer("localhost:13801", []string{"trade"})
-
+	exchanger.PushQServer("localhost:13800", []string{"trade"})
+	//临时在这里创建的SessionManager
 	time.Sleep(1 * time.Second)
+}
 
-	// 创建客户端
-	manager = client.NewKiteClientManager(":18002", "", "s-trade-a", "123456")
+func initKiteClient() {
+	//开始向服务端发送数据
+    manager = client.NewKiteClientManager(":18002", "", "s-trade-a", "123456")
 	manager.AddListener(&listener.ConsoleListener{})
 	// 设置发送类型
 	if err := manager.SetPubs([]string{"trade"}); err != nil {
@@ -119,7 +125,7 @@ func init() {
 
 func BenchmarkRemotingServer(t *testing.B) {
 	for i := 0; i < t.N; i++ {
-		err := manager.SendStringMessage(buildStringMessage())
+		err := manager.SendStringMessage(buildStringMessage("1"))
 		if nil != err {
 			t.Logf("SEND MESSAGE |FAIL|%s\n", err)
 		}
@@ -128,23 +134,24 @@ func BenchmarkRemotingServer(t *testing.B) {
 
 func TestRemotingServer(t *testing.T) {
 
-	err := manager.SendStringMessage(buildStringMessage())
+	err := manager.SendStringMessage(buildStringMessage("1"))
 	if nil != err {
 		t.Fail()
 		t.Logf("SEND MESSAGE |FAIL|%s\n", err)
 	}
 
-	err = manager.SendStringMessage(buildStringMessage())
+	err = manager.SendStringMessage(buildStringMessage("2"))
 	if nil != err {
 		t.Fail()
 		t.Logf("SEND MESSAGE |FAIL|%s\n", err)
 	}
 
-	err = manager.SendStringMessage(buildStringMessage())
+	err = manager.SendStringMessage(buildStringMessage("3"))
 	if nil != err {
 		t.Fail()
 		t.Logf("SEND MESSAGE |FAIL|%s\n", err)
 	}
 
-	time.Sleep(200 * time.Second)
+	time.Sleep(20 * time.Second)
 }
+
