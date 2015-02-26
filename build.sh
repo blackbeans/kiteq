db586b3fceb323d24cb6434579fe20087f4f41f2
blackbeans <blackbeans.zc@gmail.com>
2015-02-26 19:34:40 +0800

    增加gorp的依赖 	modified:   build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ go get github.com/golang/protobuf/{proto,protoc-gen-go}
 go get github.com/go-sql-driver/mysql
 go get github.com/blackbeans/zk
 go get github.com/BurntSushi/toml
+go get github.com/sutoo/gorp
 
 protoc --go_out=. ./protocol/*.proto
 
