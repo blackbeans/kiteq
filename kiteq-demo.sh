f778d4fc35068c49890ae0f1f8cb4f24916534ad
zhao.chao <zhao.chao@uneed.com>
2017-02-08 22:26:26 +0800

    modified:   conf/cluster.toml 	deleted:    conf/cluster_demo.toml 	deleted:    kiteq-demo.sh

--- kiteq-demo.sh
+++
@@ -1,4 +0,0 @@
-./kiteq -clusterName=file_dev -configPath=./conf/cluster_demo.toml -pport=13901 -bind=:13900
-#./kiteq -clusterName=memory_dev -configPath=./conf/cluster.toml -pport=13801 -bind=:13800
-#./kiteq  -clusterName=mysql_dev  -configPath=./conf/cluster.toml  -pport=13801 -bind=:13800
-
