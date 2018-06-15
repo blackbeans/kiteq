90f3ec3fd31519ddddb690c1935a1d55610f597b
zhao.chao <zhao.chao@uneed.com>
2018-06-15 23:58:45 +0800

    升级了turbo的tclient

--- vendor/github.com/blackbeans/turbo/tclient.go
+++ vendor/github.com/blackbeans/turbo/tclient.go
@@ -240,7 +240,6 @@ func (self *TClient) Write(p Packet) (*Future, error) {
 func (self *TClient) asyncWrite() {
 
 	go func() {
-		packets := make([]*Packet, 0, 100)
 		for !self.IsClosed() {
 
 			select {
@@ -267,38 +266,18 @@ func (self *TClient) asyncWrite() {
 						//设置数据
 						p.Data = raw
 						//其他的都OK
-						packets = append(packets, p)
 					}
-				}
-
-				if len(packets) >=100{
 					//批量写入
-					err := self.s.Write(packets...)
+					err = self.s.Write(p)
 					//链接是关闭的
 					if nil != err {
-						log.ErrorLog("stderr", "TClient|asyncWrite|Write|FAIL|%v|%d",
-							err,len(packets))
+						log.ErrorLog("stderr", "TClient|asyncWrite|Write|FAIL|%v",
+							err)
 						self.s.Close()
 						continue
 					}
-					packets = packets[:0]
 				}
-				case <-time.After(100*time.Millisecond):
-				if len(packets)>0 {
-					//批量写入
-					err := self.s.Write(packets...)
-					//链接是关闭的
-					if nil != err {
-						log.ErrorLog("stderr", "TClient|asyncWrite|Write|FAIL|%v|%d",
-							err,len(packets))
-						self.s.Close()
-						continue
-					}
-					packets = packets[:0]
-				}
-
 			}
-
 		}
 	}()
 }
